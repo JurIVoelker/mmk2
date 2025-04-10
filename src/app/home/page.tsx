09c47a0fe98664a0ce49f17fadbd4a3157fce138
niroe22 <102026015+niroe22@users.noreply.github.com>
2025-04-10 14:31:34 +0200

    BABABABA

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
 
 const Homepage = () => {
-  const t = "hallo";
-  const text = t + " Welt";
   return (
     <>
-      <h1>aoweauwe</h1>
-      <Button variant={"ghost"}>Hallo</Button>
+            Works
     </>
   );
 };
