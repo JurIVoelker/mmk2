0e411f307c6ac9fc0ed330389b21e09e81b24c9a
niroe22 <102026015+niroe22@users.noreply.github.com>
2025-04-24 14:21:54 +0200

    added news swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 "use client";
-import { DraggableCard } from "@/components/draggable-card";
-import {
-  DndContext,
-  MouseSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  const onDragEnd = () => {
-    console.log("dropped");
-  };
-
-  const touchSensor = useSensor(TouchSensor);
-
-  const mouseSensor = useSensor(MouseSensor);
-
-  const sensors = useSensors(touchSensor, mouseSensor);
-
   return (
-    <DndContext onDragEnd={onDragEnd} autoScroll={false} sensors={sensors}>
-      <div className="w-full h-full flex items-center justify-center overflow-hidden">
-        <DraggableCard />
-      </div>
-    </DndContext>
+      <>
+        <Button asChild className="w-full mx-auto">
+          <Link href={"/home"}>Swiper</Link>
+        </Button>
+      </>
+
   );
 }
