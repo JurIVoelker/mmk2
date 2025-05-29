f43c8a03bcfa1a14030626dd62b3edd9345dabf2
Gajana Kartasova <gajanakartasova@gmail.com>
2025-05-29 14:27:42 +0200

    added carousel

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -4,18 +4,28 @@ import {useState} from "react";
 import {ImageContent} from "@/components/cards-content/image-content";
 import {VideoContent} from "@/components/cards-content/video-content";
 import {TextContent} from "@/components/cards-content/text-content";
-import InfoActionButtons from "@/components/info-action-buttons";
 import CustomLayout from "@/components/custom-layout";
+import {cn} from "@/lib/utils";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 type ContentType = "image" | "video" | "newspaper";
 
 const Infopage = () => {
     const [currentContent, setCurrentContent] = useState<ContentType>("image");
 
+    const flow: ContentType[] = ["image", "video", "newspaper"];
+
     const labelMap: { [key in ContentType]: string } = {
-        image: "Instagram-Beitrag",
-        video: "Tiktok-Beitrag",
-        newspaper: "Online Zeitungsartikel",
+        image: "Instagram",
+        video: "Tiktok",
+        newspaper: "Zeitungsartikel",
     };
 
     const textMap: { [key in ContentType]: string } = {
@@ -42,49 +52,116 @@ const Infopage = () => {
             "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
     };
 
-    const renderContent = () => {
-        switch (currentContent) {
-            case "image":
-                return <ImageContent src="./assets/image/test.jpg"/>;
-            case "video":
-                return <VideoContent src="./assets/video/test.mp4"/>;
-            case "newspaper":
-                return <TextContent src="./assets/image/test.jpg" content={"Test"} date={new Date()} category={"Test"}
-                                    title={"Test"}/>;
-            default:
-                return null;
-        }
-    };
-
     return (
         <CustomLayout>
-            <div className="flex flex-col w-full h-full text-center justify-between">
-                <div className={"overflow-y-auto"}>
-                    <div className="sticky top-0 bg-white z-10 pt-4 pb-4 gap-1 flex items-center justify-center">
-                        <img src="/assets/icons/info.svg" alt="Info" className="w-6 h-6"/>
-                        <h2 className="text-xl font-bold">{labelMap[currentContent]}</h2>
-                    </div>
-                    <div className="flex flex-col gap-4 overflow-y-auto ">
-                        <div className={"p-1"}>
-                            <div className={"border border-black rounded-md h-[65vh]"}>
-                                {renderContent()}
+            <Carousel opts={{loop: true}}
+                      setApi={(api) => {
+                          api.on("select", () => {
+                              const index = api.selectedScrollSnap()
+                              const newContent = ["image", "video", "newspaper"][index] as ContentType
+                              setCurrentContent(newContent)
+                          })
+                      }}
+            >
+                <div className="flex flex-col w-full h-full text-center justify-between">
+                    <CarouselContent>
+                        <CarouselItem>
+                            {/*Carousel Item*/}
+                            <div className={"overflow-y-auto"}>
+                                <div
+                                    className="sticky top-0 bg-white z-10 pt-4 pb-4 gap-1 flex items-center justify-center">
+                                    <img src="/assets/icons/info.svg" alt="Info" className="w-6 h-6"/>
+                                    <h2 className="text-xl font-bold">Instagram-Beitrag</h2>
+                                </div>
+                                <div className="flex flex-col gap-4 overflow-y-auto ">
+                                    <div className={"p-1"}>
+                                        <div className={"border border-black rounded-md h-[65vh]"}>
+                                            <ImageContent src="./assets/image/test.jpg"/></div>
+                                    </div>
+                                    <div className="pt-4 pb-4 text-left">
+                                        <p>
+                                            {textMap["image"]}
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="pt-4 pb-4 text-left">
-                            <p>
-                                {textMap[currentContent]}
-                            </p>
-                        </div>
-                    </div>
+                        </CarouselItem>
+                        <CarouselItem>
+                            {/*Carousel Item*/}
+                            <div className={"overflow-y-auto"}>
+                                <div
+                                    className="sticky top-0 bg-white z-10 pt-4 pb-4 gap-1 flex items-center justify-center">
+                                    <img src="/assets/icons/info.svg" alt="Info" className="w-6 h-6"/>
+                                    <h2 className="text-xl font-bold">Tiktok-Beitrag</h2>
+                                </div>
+                                <div className="flex flex-col gap-4 overflow-y-auto ">
+                                    <div className={"p-1"}>
+                                        <div className={"border border-black rounded-md h-[65vh]"}>
+                                            <VideoContent src="./assets/video/test.mp4"/></div>
+                                    </div>
+                                    <div className="pt-4 pb-4 text-left">
+                                        <p>
+                                            {textMap["video"]}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                        <CarouselItem>
+                            {/*Carousel Item*/}
+                            <div className={"overflow-y-auto"}>
+                                <div
+                                    className="sticky top-0 bg-white z-10 pt-4 pb-4 gap-1 flex items-center justify-center">
+                                    <img src="/assets/icons/info.svg" alt="Info" className="w-6 h-6"/>
+                                    <h2 className="text-xl font-bold">Online Zeitungsartikel</h2>
+                                </div>
+                                <div className="flex flex-col gap-4 overflow-y-auto ">
+                                    <div className={"p-1"}>
+                                        <div className={"border border-black rounded-md h-[65vh]"}>
+                                            <TextContent src="./assets/image/test.jpg" content={"Test"}
+                                                         date={new Date()} category={"Test"}
+                                                         title={"Test"}/></div>
+                                    </div>
+                                    <div className="pt-4 pb-4 text-left">
+                                        <p>
+                                            {textMap["newspaper"]}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </CarouselItem>
+                    </CarouselContent>
                 </div>
                 <div className="sticky bottom-0 bg-white z-10">
-                    <InfoActionButtons
-                        currentContent={currentContent}
-                        onChangeContent={setCurrentContent}
-                    />
+                    <div className="flex justify-between w-full p-4 w-full">
+                        <div className="flex-1 flex justify-center">
+                            <CarouselPrevious
+                                currentContent={currentContent}
+                                contentOrder={["image", "video", "newspaper"]}
+                                labelMap={labelMap}
+                            />
+                        </div>
+                        <div className="flex-1 flex gap-2 items-center justify-center">
+                            {flow.map((item) => (
+                                <div
+                                    key={item}
+                                    className={cn(
+                                        "w-5 h-5 rounded-full transition-colors duration-300",
+                                        item === currentContent ? "bg-brown" : "bg-brown-light"
+                                    )}
+                                />
+                            ))}
+                        </div>
+                        <div className="flex-1 flex justify-center">
+                            <CarouselNext
+                                currentContent={currentContent}
+                                contentOrder={["image", "video", "newspaper"]}
+                                labelMap={labelMap}
+                            />
+                        </div>
+                    </div>
                 </div>
-
-            </div>
+            </Carousel>
         </CustomLayout>
     );
 };
