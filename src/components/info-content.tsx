ba4c5b020f1133e4b6ab27a374ad3184da467182
Gajana Kartasova <gajanakartasova@gmail.com>
2025-05-29 19:55:08 +0200

    integrated info overlay

--- src/components/info-content.tsx
+++ src/components/info-content.tsx
@@ -1,103 +1,136 @@
 "use client";
 
-import { useState } from "react";
-import { ImageContent } from "@/components/cards-content/image-content";
-import { VideoContent } from "@/components/cards-content/video-content";
-import { TextContent } from "@/components/cards-content/text-content";
-import InfoActionButtons from "@/components/info-action-buttons";
-import { Button } from "./ui/button";
-import { ChevronLeft } from "lucide-react";
+import {useState} from "react";
+import {ImageContent} from "@/components/cards-content/image-content";
+import {VideoContent} from "@/components/cards-content/video-content";
+import {TextContent} from "@/components/cards-content/text-content";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import {CarouselSlideContent} from "@/components/carousel-slide-content";
+import {cn} from "@/lib/utils";
+import {ChevronLeft} from "lucide-react";
+import {Button} from "./ui/button";
 
 type ContentType = "image" | "video" | "newspaper";
 
-const InfoContent = ({ onClose }: { onClose?: () => void }) => {
-  const [currentContent, setCurrentContent] = useState<ContentType>("image");
+const InfoContent = ({onClose}: { onClose?: () => void }) => {
+    const [currentContent, setCurrentContent] = useState<ContentType>("image");
 
-  const labelMap: { [key in ContentType]: string } = {
-    image: "Instagram-Beitrag",
-    video: "Tiktok-Beitrag",
-    newspaper: "Online Zeitungsartikel",
-  };
+    const labelMap: { [key in ContentType]: string } = {
+        image: "Instagram",
+        video: "Tiktok",
+        newspaper: "Zeitungsartikel",
+    };
 
-  const textMap: { [key in ContentType]: string } = {
-    image:
-      "Das ist eine Beschreibung zum Bild. Lorem ipsum dolor sit amet. " +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-    video:
-      "Hier geht es um ein Video. Lorem ipsum dolor sit amet." +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-    newspaper:
-      "Das ist ein Zeitungstext. Lorem ipsum dolor sit amet." +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-  };
+    const infoOrder: ContentType[] = ["image", "video", "newspaper"];
 
-  const renderContent = () => {
-    switch (currentContent) {
-      case "image":
-        return <ImageContent src="./assets/image/test.jpg" />;
-      case "video":
-        return <VideoContent src="./assets/video/test.mp4" />;
-      case "newspaper":
-        return (
-          <TextContent
-            src="./assets/image/test.jpg"
-            content={"Test"}
-            date={new Date()}
-            category={"Test"}
-            title={"Test"}
-          />
-        );
-      default:
-        return null;
-    }
-  };
+    const textMap: { [key in ContentType]: string } = {
+        image: "Das ist eine Beschreibung zum Bild. Lorem ipsum dolor sit amet. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        video: "Hier geht es um ein Video. Lorem ipsum dolor sit amet." +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+        newspaper: "Das ist ein Zeitungstext. Lorem ipsum dolor sit amet." +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+    };
 
-  return (
-    <div className="flex flex-col w-full h-full text-center justify-between">
-      <div className={"overflow-y-auto relative"}>
-        <div className="sticky top-0 bg-white z-10 pt-4 pb-4 gap-1 flex items-center justify-center">
-          {onClose && (
-            <Button onClick={onClose} className="w-9 absolute left-0">
-              <ChevronLeft />
-            </Button>
-          )}
-          <img src="/assets/icons/info.svg" alt="Info" className="w-6 h-6" />
-          <h2 className="text-xl font-bold">{labelMap[currentContent]}</h2>
-        </div>
-        <div className="flex flex-col gap-4 overflow-y-auto ">
-          <div className={"p-1"}>
-            <div className={"border border-black rounded-md h-[65vh]"}>
-              {renderContent()}
+    return (
+        <Carousel opts={{loop: true}}
+                  setApi={(api) => {
+                      if (!api) return
+                      api.on("select", () => {
+                          const index = api.selectedScrollSnap()
+                          const newContent = ["image", "video", "newspaper"][index] as ContentType
+                          setCurrentContent(newContent)
+                      })
+                  }}
+        >
+            <div className="flex flex-col w-full h-full text-center justify-between relative">
+                {/*TODO*/}
+                <div className="absolute top-0 z-20 bg-white flex items-center px-4 py-2">
+                    {onClose && (
+                        <Button onClick={onClose} className="w-9">
+                            <ChevronLeft/>
+                        </Button>
+                    )}
+                </div>
+                <CarouselContent>
+                    {infoOrder.map((type) => (
+                        <CarouselItem key={type}>
+                            <CarouselSlideContent
+                                title={labelMap[type] + (type === "newspaper" ? "" : "-Beitrag")}
+                                content={
+                                    type === "image" ? (
+                                        <ImageContent src="./assets/image/test.jpg"/>
+                                    ) : type === "video" ? (
+                                        <VideoContent src="./assets/video/test.mp4"/>
+                                    ) : (
+                                        <TextContent
+                                            src="./assets/image/test.jpg"
+                                            content="Test"
+                                            date={new Date()}
+                                            category="Test"
+                                            title="Test"
+                                        />
+                                    )
+                                }
+                                text={textMap[type]}
+                            />
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
             </div>
-          </div>
-          <div className="pt-4 pb-4 text-left">
-            <p>{textMap[currentContent]}</p>
-          </div>
-        </div>
-      </div>
-      <div className="sticky bottom-0 bg-white z-10">
-        <InfoActionButtons
-          currentContent={currentContent}
-          onChangeContent={setCurrentContent}
-        />
-      </div>
-    </div>
-  );
+            <div className="sticky bottom-0 bg-white z-10">
+                <div className="flex justify-between w-full p-4 w-full">
+                    <div className="flex-1 flex justify-center">
+                        <CarouselPrevious
+                            currentContent={currentContent}
+                            contentOrder={infoOrder}
+                            labelMap={labelMap}
+                        />
+                    </div>
+                    <div className="flex-1 flex gap-2 items-center justify-center">
+                        {infoOrder.map((item) => (
+                            <div
+                                key={item}
+                                className={cn(
+                                    "w-3 h-3 rounded-full transition-colors duration-300",
+                                    item === currentContent ? "bg-brown" : "bg-brown-light"
+                                )}
+                            />
+                        ))}
+                    </div>
+                    <div className="flex-1 flex justify-center">
+                        <CarouselNext
+                            currentContent={currentContent}
+                            contentOrder={infoOrder}
+                            labelMap={labelMap}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Carousel>
+    );
 };
 
 export default InfoContent;
+
