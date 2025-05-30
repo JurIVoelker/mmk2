20f8f1d89581d9aa4653c012f80a643fe457df52
JurIVoelker <juri.voelker@gmx.de>
2025-05-30 15:00:58 +0200

    fix(carousel): add vertical scrolling

--- src/components/info-content.tsx
+++ src/components/info-content.tsx
@@ -1,133 +1,147 @@
 "use client";
 
-import React, {useState} from "react";
-import {ImageContent} from "@/components/cards-content/image-content";
-import {VideoContent} from "@/components/cards-content/video-content";
-import {TextContent} from "@/components/cards-content/text-content";
+import React, { useState } from "react";
+import { ImageContent } from "@/components/cards-content/image-content";
+import { VideoContent } from "@/components/cards-content/video-content";
+import { TextContent } from "@/components/cards-content/text-content";
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel"
-import {CarouselSlideContent} from "@/components/carousel-slide-content";
-import {cn} from "@/lib/utils";
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { CarouselSlideContent } from "@/components/carousel-slide-content";
+import { cn } from "@/lib/utils";
 
 type ContentType = "image" | "video" | "newspaper";
 
-const InfoContent = ({onClose}: { onClose?: () => void }) => {
-    const [currentContent, setCurrentContent] = useState<ContentType>("image");
+const InfoContent = ({ onClose }: { onClose?: () => void }) => {
+  const [currentContent, setCurrentContent] = useState<ContentType>("image");
 
-    const labelMap: { [key in ContentType]: string } = {
-        image: "Instagram",
-        video: "Tiktok",
-        newspaper: "Zeitungsartikel",
-    };
+  const labelMap: { [key in ContentType]: string } = {
+    image: "Instagram",
+    video: "Tiktok",
+    newspaper: "Zeitungsartikel",
+  };
 
-    const infoOrder: ContentType[] = ["image", "video", "newspaper"];
+  const infoOrder: ContentType[] = ["image", "video", "newspaper"];
 
-    const textMap: { [key in ContentType]: string } = {
-        image: "Das ist eine Beschreibung zum Bild. Lorem ipsum dolor sit amet. " +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-        video: "Hier geht es um ein Video. Lorem ipsum dolor sit amet." +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-        newspaper: "Das ist ein Zeitungstext. Lorem ipsum dolor sit amet." +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
-            "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-    };
+  const textMap: { [key in ContentType]: string } = {
+    image:
+      "Das ist eine Beschreibung zum Bild. Lorem ipsum dolor sit amet. " +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+    video:
+      "Hier geht es um ein Video. Lorem ipsum dolor sit amet." +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+    newspaper:
+      "Das ist ein Zeitungstext. Lorem ipsum dolor sit amet." +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.\n" +
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.\n" +
+      "Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+  };
 
-    return (
-        <Carousel opts={{loop: true}}
-                  setApi={(api) => {
-                      if (!api) return
-                      api.on("select", () => {
-                          const index = api.selectedScrollSnap()
-                          const newContent = ["image", "video", "newspaper"][index] as ContentType
-                          setCurrentContent(newContent)
-                      })
-                  }}
-        >
-            <div className="flex flex-col w-full h-full text-center justify-between relative">
-                <div className="absolute top-0 z-20 bg-white flex items-center p-4">
-                    {onClose && (
-                        <button onClick={onClose} className="transition-transform cursor-pointer hover:-translate-x-1">
-                            <img src="/assets/icons/arrow-left.svg" alt="Zurück" className="w-6 h-6"/>
-                        </button>
-                    )}
-                </div>
-                <CarouselContent>
-                    {infoOrder.map((type) => (
-                        <CarouselItem key={type}>
-                            <CarouselSlideContent
-                                title={labelMap[type] + (type === "newspaper" ? "" : "-Beitrag")}
-                                content={
-                                    type === "image" ? (
-                                        <ImageContent src="./assets/image/test.jpg"/>
-                                    ) : type === "video" ? (
-                                        <VideoContent src="./assets/video/test.mp4"/>
-                                    ) : (
-                                        <TextContent
-                                            src="./assets/image/test.jpg"
-                                            content="Test"
-                                            date={new Date()}
-                                            category="Test"
-                                            title="Test"
-                                        />
-                                    )
-                                }
-                                text={textMap[type]}
-                            />
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-            </div>
-            <div className="sticky bottom-0 bg-white z-10">
-                <div className="flex justify-between w-full p-4 w-full">
-                    <div className="flex-1 flex justify-center">
-                        <CarouselPrevious
-                            currentContent={currentContent}
-                            contentOrder={infoOrder}
-                            labelMap={labelMap}
-                        />
-                    </div>
-                    <div className="flex-1 flex gap-2 items-center justify-center">
-                        {infoOrder.map((item) => (
-                            <div
-                                key={item}
-                                className={cn(
-                                    "w-3 h-3 rounded-full transition-colors duration-300",
-                                    item === currentContent ? "bg-brown" : "bg-brown-light"
-                                )}
-                            />
-                        ))}
-                    </div>
-                    <div className="flex-1 flex justify-center">
-                        <CarouselNext
-                            currentContent={currentContent}
-                            contentOrder={infoOrder}
-                            labelMap={labelMap}
-                        />
-                    </div>
-                </div>
-            </div>
-        </Carousel>
-    );
+  return (
+    <Carousel
+      opts={{ loop: true }}
+      setApi={(api) => {
+        if (!api) return;
+        api.on("select", () => {
+          const index = api.selectedScrollSnap();
+          const newContent = ["image", "video", "newspaper"][
+            index
+          ] as ContentType;
+          setCurrentContent(newContent);
+        });
+      }}
+    >
+      <div className="flex flex-col w-full h-full text-center justify-between relative">
+        <div className="absolute top-0 z-20 bg-white flex items-center p-4">
+          {onClose && (
+            <button
+              onClick={onClose}
+              className="transition-transform cursor-pointer hover:-translate-x-1"
+            >
+              <img
+                src="/assets/icons/arrow-left.svg"
+                alt="Zurück"
+                className="w-6 h-6"
+              />
+            </button>
+          )}
+        </div>
+        <CarouselContent>
+          {infoOrder.map((type) => (
+            <CarouselItem key={type}>
+              <CarouselSlideContent
+                title={
+                  labelMap[type] + (type === "newspaper" ? "" : "-Beitrag")
+                }
+                content={
+                  type === "image" ? (
+                    <ImageContent src="./assets/image/test.jpg" />
+                  ) : type === "video" ? (
+                    <VideoContent src="./assets/video/test.mp4" />
+                  ) : (
+                    <TextContent
+                      src="./assets/image/test.jpg"
+                      content="Test"
+                      date={new Date()}
+                      category="Test"
+                      title="Test"
+                    />
+                  )
+                }
+                text={textMap[type]}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </div>
+      <div className="sticky bottom-0 bg-white z-10">
+        <div className="flex justify-between p-4 w-full">
+          <div className="flex-1 flex justify-center">
+            <CarouselPrevious
+              currentContent={currentContent}
+              contentOrder={infoOrder}
+              labelMap={labelMap}
+            />
+          </div>
+          <div className="flex-1 flex gap-2 items-center justify-center">
+            {infoOrder.map((item) => (
+              <div
+                key={item}
+                className={cn(
+                  "w-3 h-3 rounded-full transition-colors duration-300",
+                  item === currentContent ? "bg-brown" : "bg-brown-light"
+                )}
+              />
+            ))}
+          </div>
+          <div className="flex-1 flex justify-center">
+            <CarouselNext
+              currentContent={currentContent}
+              contentOrder={infoOrder}
+              labelMap={labelMap}
+            />
+          </div>
+        </div>
+      </div>
+    </Carousel>
+  );
 };
 
 export default InfoContent;
-
--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,43 +1,43 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
-} from "embla-carousel-react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+} from "embla-carousel-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-type CarouselApi = UseEmblaCarouselType[1]
-type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
-type CarouselOptions = UseCarouselParameters[0]
-type CarouselPlugin = UseCarouselParameters[1]
+type CarouselApi = UseEmblaCarouselType[1];
+type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
+type CarouselOptions = UseCarouselParameters[0];
+type CarouselPlugin = UseCarouselParameters[1];
 
 type CarouselProps = {
-  opts?: CarouselOptions
-  plugins?: CarouselPlugin
-  orientation?: "horizontal" | "vertical"
-  setApi?: (api: CarouselApi) => void
-}
+  opts?: CarouselOptions;
+  plugins?: CarouselPlugin;
+  orientation?: "horizontal" | "vertical";
+  setApi?: (api: CarouselApi) => void;
+};
 
 type CarouselContextProps = {
-  carouselRef: ReturnType<typeof useEmblaCarousel>[0]
-  api: ReturnType<typeof useEmblaCarousel>[1]
-  scrollPrev: () => void
-  scrollNext: () => void
-  canScrollPrev: boolean
-  canScrollNext: boolean
-} & CarouselProps
+  carouselRef: ReturnType<typeof useEmblaCarousel>[0];
+  api: ReturnType<typeof useEmblaCarousel>[1];
+  scrollPrev: () => void;
+  scrollNext: () => void;
+  canScrollPrev: boolean;
+  canScrollNext: boolean;
+} & CarouselProps;
 
-const CarouselContext = React.createContext<CarouselContextProps | null>(null)
+const CarouselContext = React.createContext<CarouselContextProps | null>(null);
 
 function useCarousel() {
-  const context = React.useContext(CarouselContext)
+  const context = React.useContext(CarouselContext);
 
   if (!context) {
-    throw new Error("useCarousel must be used within a <Carousel />")
+    throw new Error("useCarousel must be used within a <Carousel />");
   }
 
-  return context
+  return context;
 }
 
 function Carousel({
@@ -55,52 +55,52 @@ function Carousel({
       axis: orientation === "horizontal" ? "x" : "y",
     },
     plugins
-  )
-  const [canScrollPrev, setCanScrollPrev] = React.useState(false)
-  const [canScrollNext, setCanScrollNext] = React.useState(false)
+  );
+  const [canScrollPrev, setCanScrollPrev] = React.useState(false);
+  const [canScrollNext, setCanScrollNext] = React.useState(false);
 
   const onSelect = React.useCallback((api: CarouselApi) => {
-    if (!api) return
-    setCanScrollPrev(api.canScrollPrev())
-    setCanScrollNext(api.canScrollNext())
-  }, [])
+    if (!api) return;
+    setCanScrollPrev(api.canScrollPrev());
+    setCanScrollNext(api.canScrollNext());
+  }, []);
 
   const scrollPrev = React.useCallback(() => {
-    api?.scrollPrev()
-  }, [api])
+    api?.scrollPrev();
+  }, [api]);
 
   const scrollNext = React.useCallback(() => {
-    api?.scrollNext()
-  }, [api])
+    api?.scrollNext();
+  }, [api]);
 
   const handleKeyDown = React.useCallback(
     (event: React.KeyboardEvent<HTMLDivElement>) => {
       if (event.key === "ArrowLeft") {
-        event.preventDefault()
-        scrollPrev()
+        event.preventDefault();
+        scrollPrev();
       } else if (event.key === "ArrowRight") {
-        event.preventDefault()
-        scrollNext()
+        event.preventDefault();
+        scrollNext();
       }
     },
     [scrollPrev, scrollNext]
-  )
+  );
 
   React.useEffect(() => {
-    if (!api || !setApi) return
-    setApi(api)
-  }, [api, setApi])
+    if (!api || !setApi) return;
+    setApi(api);
+  }, [api, setApi]);
 
   React.useEffect(() => {
-    if (!api) return
-    onSelect(api)
-    api.on("reInit", onSelect)
-    api.on("select", onSelect)
+    if (!api) return;
+    onSelect(api);
+    api.on("reInit", onSelect);
+    api.on("select", onSelect);
 
     return () => {
-      api?.off("select", onSelect)
-    }
-  }, [api, onSelect])
+      api?.off("select", onSelect);
+    };
+  }, [api, onSelect]);
 
   return (
     <CarouselContext.Provider
@@ -127,16 +127,16 @@ function Carousel({
         {children}
       </div>
     </CarouselContext.Provider>
-  )
+  );
 }
 
 function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
-  const { carouselRef, orientation } = useCarousel()
+  const { carouselRef, orientation } = useCarousel();
 
   return (
     <div
       ref={carouselRef}
-      className="overflow-hidden"
+      className="overflow-scroll scrollbar-hide"
       data-slot="carousel-content"
     >
       <div
@@ -148,11 +148,11 @@ function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
         {...props}
       />
     </div>
-  )
+  );
 }
 
 function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
-  const { orientation } = useCarousel()
+  const { orientation } = useCarousel();
 
   return (
     <div
@@ -166,81 +166,90 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
       )}
       {...props}
     />
-  )
+  );
 }
 
-type ContentType = "image" | "video" | "newspaper"
+type ContentType = "image" | "video" | "newspaper";
 
 type CarouselDirectionButtonProps = React.ComponentProps<typeof Button> & {
-  currentContent: ContentType
-  contentOrder: ContentType[]
-  labelMap: Record<ContentType, string>
-}
+  currentContent: ContentType;
+  contentOrder: ContentType[];
+  labelMap: Record<ContentType, string>;
+};
 
 function CarouselPrevious({
-                            className,
-                            currentContent,
-                            contentOrder,
-                            labelMap,
-                            ...props
-                          }: CarouselDirectionButtonProps) {
-  const { scrollPrev, canScrollPrev } = useCarousel()
+  className,
+  currentContent,
+  contentOrder,
+  labelMap,
+  ...props
+}: CarouselDirectionButtonProps) {
+  const { scrollPrev, canScrollPrev } = useCarousel();
 
-  const currentIndex = contentOrder.indexOf(currentContent)
-  const prevIndex = (currentIndex - 1 + contentOrder.length) % contentOrder.length
-  const prevLabel = labelMap[contentOrder[prevIndex]]
+  const currentIndex = contentOrder.indexOf(currentContent);
+  const prevIndex =
+    (currentIndex - 1 + contentOrder.length) % contentOrder.length;
+  const prevLabel = labelMap[contentOrder[prevIndex]];
 
   return (
-      <div className="flex flex-col items-center">
-        <button
-            data-slot="carousel-previous"
-            className={cn(
-                "cursor-pointer rounded-full flex items-center justify-center transition-transform hover:-translate-x-1",
-                className
-            )}
-            disabled={!canScrollPrev}
-            onClick={scrollPrev}
-            {...props}
-        >
-          <img src="/assets/icons/arrow-left-circle.svg" alt="Zurück" className="w-10 h-10"/>
-          <span className="sr-only">Previous slide</span>
-        </button>
-        <span className="text-black mt-1">{prevLabel}</span>
-      </div>
-  )
+    <div className="flex flex-col items-center">
+      <button
+        data-slot="carousel-previous"
+        className={cn(
+          "cursor-pointer rounded-full flex items-center justify-center transition-transform hover:-translate-x-1",
+          className
+        )}
+        disabled={!canScrollPrev}
+        onClick={scrollPrev}
+        {...props}
+      >
+        <img
+          src="/assets/icons/arrow-left-circle.svg"
+          alt="Zurück"
+          className="w-10 h-10"
+        />
+        <span className="sr-only">Previous slide</span>
+      </button>
+      <span className="text-black mt-1">{prevLabel}</span>
+    </div>
+  );
 }
 
 function CarouselNext({
-                        className,
-                        currentContent,
-                        contentOrder,
-                        labelMap,
-                        ...props
-                      }: CarouselDirectionButtonProps) {
-  const { scrollNext, canScrollNext } = useCarousel()
+  className,
+  currentContent,
+  contentOrder,
+  labelMap,
+  ...props
+}: CarouselDirectionButtonProps) {
+  const { scrollNext, canScrollNext } = useCarousel();
 
-  const currentIndex = contentOrder.indexOf(currentContent)
-  const nextIndex = (currentIndex + 1) % contentOrder.length
-  const nextLabel = labelMap[contentOrder[nextIndex]]
+  const currentIndex = contentOrder.indexOf(currentContent);
+  const nextIndex = (currentIndex + 1) % contentOrder.length;
+  const nextLabel = labelMap[contentOrder[nextIndex]];
 
   return (
-      <div className="flex flex-col items-center">
-        <button
-            data-slot="carousel-next"
-            className={cn(
-                "cursor-pointer flex items-center justify-center transition-transform hover:translate-x-1",
-                className
-            )}
-            disabled={!canScrollNext}
-            onClick={scrollNext}
-            {...props}
-        >
-          <img src="/assets/icons/arrow-right-circle.svg" alt="Weiter" className="w-10 h-10"/>
-          <span className="sr-only">Next slide</span>
-        </button>
-        <span className="text-black mt-1">{nextLabel}</span>
-      </div>
-  )
+    <div className="flex flex-col items-center">
+      <button
+        data-slot="carousel-next"
+        className={cn(
+          "cursor-pointer flex items-center justify-center transition-transform hover:translate-x-1",
+          className
+        )}
+        disabled={!canScrollNext}
+        onClick={scrollNext}
+        {...props}
+      >
+        <img
+          src="/assets/icons/arrow-right-circle.svg"
+          alt="Weiter"
+          className="w-10 h-10"
+        />
+        <span className="sr-only">Next slide</span>
+      </button>
+      <span className="text-black mt-1">{nextLabel}</span>
+    </div>
+  );
 }
 
 export {
@@ -250,4 +259,4 @@ export {
   CarouselItem,
   CarouselPrevious,
   CarouselNext,
-}
+};
